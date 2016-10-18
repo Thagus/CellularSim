7e82395f4147913282684d297a9126449557a322
Thagus <agu.alcarin@gmail.com>
2016-10-18 17:31:53 -0500

    User component

---
+++ src/main/java/components/UserComponent.java
@@ -0,0 +1,19 @@
+package components;
+
+import com.badlogic.ashley.core.Component;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+/**
+ * Created by Thagus on 18/10/16.
+ */
+public class UserComponent implements Component {
+    public Circle user;
+    public int speed;
+
+    public UserComponent(Circle user) {
+        this.user = user;
+        this.user.setFill(Color.RED);
+        this.speed = 5*60;
+    }
+}
