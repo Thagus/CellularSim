c2e2e099ffbe29e8aae656f72e782cd748ea8c94
Thagus <agu.alcarin@gmail.com>
2016-10-18 19:56:23 -0500

    User commitments component

---
+++ src/main/java/components/UserCommitmentComponent.java
@@ -0,0 +1,12 @@
+package components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by Thagus on 18/10/16.
+ */
+public class UserCommitmentComponent implements Component {
+    public int xCommitment, yCommitment;
+    public int xDestination, yDestination;
+    public CityBlockComponent.BlockType destination;
+}
