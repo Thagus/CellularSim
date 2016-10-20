5ef0d0a9e10940467397ec41dedaefff6a771074
Thagus <agu.alcarin@gmail.com>
2016-10-20 12:40:13 -0500

    Calculate times in range

--- src/main/java/dataObjects/Profile.java
+++ src/main/java/dataObjects/Profile.java
@@ -3,8 +3,9 @@ package dataObjects;
 import components.CityBlockComponent;
 import javafx.util.Pair;
 
-import java.io.File;
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
@@ -12,10 +13,11 @@ import java.util.LinkedHashMap;
  * Created by Thagus on 20/10/16.
  */
 public class Profile {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
     private String profileName;
     private CityBlockComponent.BlockType workArea;
 
-    private LinkedHashMap<Time, ArrayList<Pair<CityBlockComponent.BlockType, Double>>> schedule;
+    private LinkedHashMap<Time, ArrayList<Pair<String, Double>>> schedule;
 
     public Profile(String name, String workArea){
         this.profileName = name;
@@ -41,16 +43,41 @@ public class Profile {
                 break;
         }
 
+        schedule = new LinkedHashMap<>();
+
         System.out.println(this.profileName + " " + this.workArea);
     }
 
     public void addScheduleEntry(String start, String end, ArrayList<Pair<String, Double>> places){
-        //Receive start and end
-        System.out.println("\t\t" + start + " - " + end);
-        //Receive places
-        for(Pair<String, Double> place : places){
-            System.out.println("\t\t\t" + place.getKey() + " - " + place.getValue());
+        ArrayList<Time> times = null;
+
+        try {
+            times = getTimesInRange(new Time(sdf.parse(start).getTime()), new Time(sdf.parse(end).getTime()));
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+
+        if(times!=null){
+            for(Time time : times){
+                //Add for each time the schedule in that range
+                schedule.put(time, places);
+            }
+        }
+    }
+
+    private ArrayList<Time> getTimesInRange(Time start, Time end){
+        ArrayList<Time> times = new ArrayList<>();
+
+        long time = start.getTime();
+        times.add(start);
+
+        //Divide the hours in intervals of 30 minutes
+        while(time<end.getTime()){
+            time += 1800000L;
+            times.add(new Time(time));
+        }
+
+        return times;
     }
 
 }
