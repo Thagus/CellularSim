d180c36e61f7c081fcbecc8b6ac1c59d7776c03d
Thagus <agu.alcarin@gmail.com>
2016-10-20 23:56:52 -0500

    Set user circle at home

--- src/main/java/components/ProfileComponent.java
+++ src/main/java/components/ProfileComponent.java
@@ -2,6 +2,8 @@ package components;
 
 import com.badlogic.ashley.core.Component;
 import dataObjects.Profile;
+import javafx.scene.shape.Circle;
+import utils.Constants;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,12 +16,14 @@ public class ProfileComponent implements Component{
     public CityBlockComponent home, work;
     public Profile profile;
 
-    public ProfileComponent(Profile profile, HashMap<CityBlockComponent.BlockType, ArrayList<CityBlockComponent>> cityBlocksIndex){
+    public ProfileComponent(Profile profile, HashMap<CityBlockComponent.BlockType, ArrayList<CityBlockComponent>> cityBlocksIndex, Circle circleToReposition){
         this.profile = profile;
         Random random = new Random();
 
         //Select the home of the user
         home = cityBlocksIndex.get(CityBlockComponent.BlockType.RESIDENTIAL).get(random.nextInt(cityBlocksIndex.get(CityBlockComponent.BlockType.RESIDENTIAL).size()));
+        circleToReposition.setCenterX(home.block.getX() + Constants.BLOCK_SIZE/2);
+        circleToReposition.setCenterY(home.block.getY() + Constants.BLOCK_SIZE/2);
 
         //Define the work block
         if(profile.isHomeWorker()){ //If he/she works from home, the work block ant the home block are the same
