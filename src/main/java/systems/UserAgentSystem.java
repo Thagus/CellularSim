b404d94c775ca8b419554fe24a9d42893c303f34
Thagus <agu.alcarin@gmail.com>
2016-10-20 23:58:31 -0500

    Agents now decide where to go based on their profiles

--- src/main/java/systems/UserAgentSystem.java
+++ src/main/java/systems/UserAgentSystem.java
@@ -4,10 +4,18 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.badlogic.gdx.math.Vector2;
+import components.CityBlockComponent;
+import components.ProfileComponent;
 import components.UserCommitmentComponent;
 import components.UserComponent;
+import javafx.util.Pair;
+import utils.Constants;
 
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -15,44 +23,141 @@ import java.util.Random;
  */
 public class UserAgentSystem extends IteratingSystem {
     private ComponentMapper<UserCommitmentComponent> ucc = ComponentMapper.getFor(UserCommitmentComponent.class);
+    private ComponentMapper<ProfileComponent> pc = ComponentMapper.getFor(ProfileComponent.class);
+    private ComponentMapper<UserComponent> uc = ComponentMapper.getFor(UserComponent.class);
 
+    private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+    private HashMap<CityBlockComponent.BlockType, ArrayList<CityBlockComponent>> cityBlocksIndex;
+    private char[] possibleMovements = {'U', 'D', 'R', 'L'};
     private Random rnd;
 
-    public UserAgentSystem() {
-        super(Family.all(UserComponent.class).get());
+    public UserAgentSystem(HashMap<CityBlockComponent.BlockType, ArrayList<CityBlockComponent>> cityBlocksIndex) {
+        super(Family.all(UserCommitmentComponent.class, ProfileComponent.class, UserComponent.class).get());
         rnd = new Random();
+        this.cityBlocksIndex = cityBlocksIndex;
     }
 
     @Override
     protected void processEntity(Entity entity, float v) {
         UserCommitmentComponent userCommitmentComponent = ucc.get(entity);
 
-        //if no commitments, check if there is a commited destination
-        //  if there is a commited destination, move to calculate best commitment
-        //  if there is no destination
-        //      based on the hour, identify the place where it should be
-        //      if the place is in traffic, identify the next destination
-
-        //Calculate possible movement (up,down,right,left) ending position
-        //Compute the Manhattan distance for each possible movement
-        //Commit to the best movement
-
         if(userCommitmentComponent.xCommitment==0 && userCommitmentComponent.yCommitment==0) {
-            if (rnd.nextBoolean()) {  //We move in X
-                if (rnd.nextDouble() > 0.2) {   //We move to the right
-                    userCommitmentComponent.xCommitment = 10;
-                } else {   //We move to the left
-                    userCommitmentComponent.xCommitment = -10;
+            //If we dont have a selected destination, select one based on the schedule
+            if(userCommitmentComponent.destination==null){
+                ProfileComponent profileComponent = pc.get(entity);
+
+                //Identify the current hour interval
+                String[] hours = Constants.clock.toString().split(":");
+                int hour = Integer.parseInt(hours[0]);
+                int minutes = Integer.parseInt(hours[1]);
+
+                String currentInterval;
+                currentInterval = (minutes>=30)? hour+":30:00" : hour+":00:00"; //If the minutes is greater than 30, the block is 30, if not the time interval is the 00
+
+                ArrayList<Pair<String, Double>> placesProbabilities;
+
+                try {
+                    placesProbabilities = profileComponent.profile.getSchedule().get(new Time(sdf.parse(currentInterval).getTime()));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                    return;
                 }
-            } else {  //We move in Y
-                if (rnd.nextDouble() > 0.2) {   //We move down
-                    userCommitmentComponent.yCommitment = 10;
-                } else {   //We move up
-                    userCommitmentComponent.yCommitment = -10;
+
+                for(Pair<String, Double> placeProbability : placesProbabilities){
+                    if(rnd.nextDouble()<placeProbability.getValue()){   //if the random value is smaller than the wanted probability, we found the place we want to be :)
+                        switch(placeProbability.getKey()){
+                            case "home":
+                                userCommitmentComponent.destination = profileComponent.home;
+                                break;
+                            case "work":
+                                if(profileComponent.work==null){    //If the work is null, the profession involves moving arround the city
+                                    //Select a random location
+                                    ArrayList<CityBlockComponent> blockComponents = cityBlocksIndex.get(CityBlockComponent.BlockType.randomBlock());
+                                    userCommitmentComponent.destination = blockComponents.get(rnd.nextInt(blockComponents.size()));
+                                }
+                                else {
+                                    userCommitmentComponent.destination = profileComponent.work;
+                                }
+                                break;
+                            case "business":
+                                userCommitmentComponent.destination = cityBlocksIndex.get(CityBlockComponent.BlockType.BUSINESS).get(rnd.nextInt(cityBlocksIndex.get(CityBlockComponent.BlockType.BUSINESS).size()));
+                                break;
+                            case "residential":
+                                userCommitmentComponent.destination = cityBlocksIndex.get(CityBlockComponent.BlockType.RESIDENTIAL).get(rnd.nextInt(cityBlocksIndex.get(CityBlockComponent.BlockType.RESIDENTIAL).size()));
+                                break;
+                            case "shopping":
+                                userCommitmentComponent.destination = cityBlocksIndex.get(CityBlockComponent.BlockType.SHOPPING).get(rnd.nextInt(cityBlocksIndex.get(CityBlockComponent.BlockType.SHOPPING).size()));
+                                break;
+                            case "park":
+                                userCommitmentComponent.destination = cityBlocksIndex.get(CityBlockComponent.BlockType.PARK).get(rnd.nextInt(cityBlocksIndex.get(CityBlockComponent.BlockType.PARK).size()));
+                                break;
+                        }
+                    }
                 }
             }
+            else {   //We already have a selected destination, make a specific commitment to improve Manhattan distance
+                //Get the current position
+                UserComponent userComponent = uc.get(entity);
 
-            //When in traffic, random wandering, unless we have a speccific destination
+                //Check if we satisfied the commitment
+                if(manhattanDistance(userCommitmentComponent.destination.block.getX() + Constants.BLOCK_SIZE / 2, userComponent.userPosition.getCenterX(), userCommitmentComponent.destination.block.getY() + Constants.BLOCK_SIZE / 2, userComponent.userPosition.getCenterY())==0){
+                    userCommitmentComponent.destination = null;
+                    return;
+                }
+
+                //Calculate possible moves
+                char bestCommitment = 'D';
+                int bestDistance = -1;
+
+                for (char move : possibleMovements) {
+                    int movementY = 0;
+                    int movementX = 0;
+                    switch (move) {
+                        case 'U':
+                            movementY = -10;
+                            break;
+                        case 'D':
+                            movementY = 10;
+                            break;
+                        case 'R':
+                            movementX = 10;
+                            break;
+                        case 'L':
+                            movementX = -10;
+                            break;
+                    }
+
+                    int newX = (int) (userComponent.userPosition.getCenterX() + movementX);
+                    int newY = (int) (userComponent.userPosition.getCenterY() + movementY);
+
+                    int distance = manhattanDistance(userCommitmentComponent.destination.block.getX() + Constants.BLOCK_SIZE / 2, newX, userCommitmentComponent.destination.block.getY() + Constants.BLOCK_SIZE / 2, newY);
+
+                    if (bestDistance == -1 || bestDistance > distance) {
+                        bestDistance = distance;
+                        bestCommitment = move;
+                    }
+                    if(bestDistance==distance && rnd.nextBoolean()){
+                        bestDistance = distance;
+                        bestCommitment = move;
+                    }
+                }
+
+                switch (bestCommitment) {
+                    case 'U':
+                        userCommitmentComponent.yCommitment = -10;
+                        break;
+                    case 'D':
+                        userCommitmentComponent.yCommitment = 10;
+                        break;
+                    case 'R':
+                        userCommitmentComponent.xCommitment = 10;
+                        break;
+                    case 'L':
+                        userCommitmentComponent.xCommitment = -10;
+                        break;
+                }
+            }
         }
     }
 
@@ -64,7 +169,7 @@ public class UserAgentSystem extends IteratingSystem {
      * @param currentY the current Y coordinate
      * @return the Manhattan distance
      */
-    private static int manhattanDistance(int targetX, int currentX, int targetY, int currentY){
-        return Math.abs(targetX-currentX) + Math.abs(targetY-currentY);
+    private static int manhattanDistance(double targetX, double currentX, double targetY, double currentY){
+        return (int) (Math.abs(targetX-currentX) + Math.abs(targetY-currentY));
     }
 }
