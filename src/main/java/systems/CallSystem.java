3d3e6a3df96875a22a73c6c933026c8bae966bf1
Thagus <agu.alcarin@gmail.com>
2016-11-17 13:30:31 -0600

    Calling system rename

--- src/main/java/systems/CallSystem.java
+++ src/main/java/systems/CallSystem.java
@@ -5,6 +5,6 @@ import com.badlogic.ashley.core.EntitySystem;
 /**
  * Created by Thagus on 18/10/16.
  */
-public class CallingSystem extends EntitySystem {
+public class CallSystem extends EntitySystem {
 
 }
