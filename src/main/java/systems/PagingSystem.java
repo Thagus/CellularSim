706ac707ff84a3612d6e6257d7725276e98466a5
Thagus <agu.alcarin@gmail.com>
2016-11-24 10:37:10 -0600

    Paging system

---
+++ src/main/java/systems/PagingSystem.java
@@ -0,0 +1,44 @@
+package systems;
+
+import com.badlogic.ashley.core.*;
+import com.badlogic.ashley.utils.ImmutableArray;
+import components.CellComponent;
+import components.UserComponent;
+import java.util.HashMap;
+
+/**
+ * Created by Thagus on 24/11/16.
+ */
+public class PagingSystem extends EntitySystem {
+    private ImmutableArray<Entity> userEntities;
+    private ImmutableArray<Entity> cellEntities;
+
+    private HashMap<Integer, Entity> paging;
+
+    private ComponentMapper<UserComponent> ucm = ComponentMapper.getFor(UserComponent.class);
+    private ComponentMapper<CellComponent> ccm = ComponentMapper.getFor(CellComponent.class);
+
+    public PagingSystem(){
+        super();
+        paging = new HashMap<>();
+    }
+
+    public void addedToEngine(Engine engine){
+        userEntities = engine.getEntitiesFor(Family.all(UserComponent.class).get());
+        cellEntities = engine.getEntitiesFor(Family.all(CellComponent.class).get());
+    }
+
+    public void update(float delta){
+
+    }
+
+    //Check if the selected user is indeed inside the paged cell
+    public boolean confirmPosition(Entity user){
+        UserComponent userComponent = ucm.get(user);
+
+        Entity cell = paging.get(userComponent.id);
+        CellComponent cellComponent = ccm.get(cell);
+
+        return cellComponent.cellCoverage.contains(userComponent.userPosition.getCenterX(), userComponent.userPosition.getCenterY());
+    }
+}
