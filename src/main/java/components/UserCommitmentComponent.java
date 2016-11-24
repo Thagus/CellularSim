1d30f9437ad66322a8452f56afb881e2b9d38b07
Thagus <agu.alcarin@gmail.com>
2016-11-24 10:29:55 -0600

    Commited time block attribute

--- src/main/java/components/UserCommitmentComponent.java
+++ src/main/java/components/UserCommitmentComponent.java
@@ -8,4 +8,7 @@ import com.badlogic.ashley.core.Component;
 public class UserCommitmentComponent implements Component {
     public float xCommitment, yCommitment;
     public CityBlockComponent destination;
+
+    //Each schedule block has a timeBlock identifier, therefore we can store the time block corresponding to the current/last commitment so we don't make many decisions per time block
+    public int commitmentTimeBlock = -1;
 }
