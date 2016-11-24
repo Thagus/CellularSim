a7f289e87f0da6e0fcb88b10824fb61e30fe4c6c
Thagus <agu.alcarin@gmail.com>
2016-11-24 10:29:12 -0600

    Added a cell ID

--- src/main/java/components/CellComponent.java
+++ src/main/java/components/CellComponent.java
@@ -9,9 +9,11 @@ import javafx.scene.shape.Circle;
  */
 public class CellComponent implements Component {
     public Circle cellCoverage;
+    public int id;
 
-    public CellComponent(Circle cellCoverage) {
+    public CellComponent(Circle cellCoverage, int id) {
         this.cellCoverage = cellCoverage;
         this.cellCoverage.setFill(new Color(135/255, 206/255, 250/255, 0.3));
+        this.id = id;
     }
 }
