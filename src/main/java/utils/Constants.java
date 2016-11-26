699e635325b9da020dfffec1b8c7beb63453868a
Thagus <agu.alcarin@gmail.com>
2016-11-26 14:20:24 -0600

    Added technology dependant constants

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -7,9 +7,15 @@ import java.sql.Time;
  */
 public class Constants {
     public static int BLOCK_SIZE = 10;
-    public static int CELL_BLOCK_RADIUS = 5;
-
     public static int NUMBER_OF_USERS = 1000;
 
+    /**
+     * Technology dependant variables
+     */
+    public static int CELL_CALL_LIMIT = 100;
+    public static int CELL_BLOCK_RADIUS = 5;
+
     public static volatile Time clock;
+
+
 }
