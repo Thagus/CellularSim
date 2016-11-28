0bd0798bb8d62a184e4af6157f2202ca59a0f50b
Thagus <agu.alcarin@gmail.com>
2016-11-28 13:01:41 -0600

    Separated main view from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,17 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
 import javafx.stage.Stage;
-import view.SimulationView;
+import view.View;
 
 /**
  * Created by Thagus on 18/10/16.
  */
 public class Main extends Application{
-
-
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Cellular simulator");
 
-        VBox layout = new VBox();
-        layout.setSpacing(5);
+        View view = new View();
 
-        layout.getChildren().add(createMenus());
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(new SimulationView(null));
-        borderPane.setLeft(createLeftMenu());
-
-        layout.getChildren().add(borderPane);
-
-
-        Scene scene = new Scene(layout, 1280, 720);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(view.createScene());
         primaryStage.show();
-    }
-
-    private MenuBar createMenus(){
-        MenuBar menuBar = new MenuBar();
-
-        //File menu
-        Menu menuFile = new Menu("_File");
-
-        MenuItem importMap = new MenuItem("Import map...");
-        menuFile.getItems().add(importMap);
-
-        menuBar.getMenus().add(menuFile);
-        return  menuBar;
-    }
-
-    private VBox createLeftMenu(){
-        VBox leftMenu = new VBox();
-        leftMenu.setAlignment(Pos.TOP_LEFT);
-        leftMenu.setSpacing(10);
-        leftMenu.setPadding(new Insets(10, 7, 5, 7));
-
-        BorderStrokeStyle boderStyle = new BorderStrokeStyle(StrokeType.INSIDE, StrokeLineJoin.MITER, StrokeLineCap.BUTT, 10, 0, null);
-        BorderStroke borderStroke = new BorderStroke(Color.LIGHTGRAY, boderStyle, CornerRadii.EMPTY, new BorderWidths(2), new Insets(2));
-        leftMenu.setBorder(new Border(borderStroke));
-
-        leftMenu.setPrefSize(256, 500);
-
-
-        return leftMenu;
     }
 }
---
+++ src/main/java/view/View.java
@@ -0,0 +1,82 @@
+package view;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.StrokeLineJoin;
+import javafx.scene.shape.StrokeType;
+import javafx.stage.Stage;
+
+/**
+ * Created by Thagus on 28/11/16.
+ */
+public class View {
+    Label receiverCellBlockedCalls, senderCellBlockedCalls, locationBlockedCalls, successfullyStartedCalls, successfullyEndedCalls;
+
+    public Scene createScene(){
+        VBox layout = new VBox();
+        layout.setSpacing(5);
+
+        layout.getChildren().add(createMenus());
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(new SimulationView(null));
+        borderPane.setLeft(createLeftMenu());
+
+        layout.getChildren().add(borderPane);
+
+
+        return new Scene(layout, 1280, 720);
+    }
+
+    private MenuBar createMenus(){
+        MenuBar menuBar = new MenuBar();
+
+        //File menu
+        Menu menuFile = new Menu("_File");
+
+        MenuItem importMap = new MenuItem("Import map...");
+        menuFile.getItems().add(importMap);
+
+        menuBar.getMenus().add(menuFile);
+        return  menuBar;
+    }
+
+    private VBox createLeftMenu(){
+        VBox leftMenu = new VBox();
+        leftMenu.setAlignment(Pos.TOP_LEFT);
+        leftMenu.setSpacing(10);
+        leftMenu.setPadding(new Insets(10, 7, 5, 7));
+
+        BorderStrokeStyle boderStyle = new BorderStrokeStyle(StrokeType.INSIDE, StrokeLineJoin.MITER, StrokeLineCap.BUTT, 10, 0, null);
+        BorderStroke borderStroke = new BorderStroke(Color.LIGHTGRAY, boderStyle, CornerRadii.EMPTY, new BorderWidths(2), new Insets(2));
+        leftMenu.setBorder(new Border(borderStroke));
+
+        leftMenu.setPrefSize(256, 500);
+
+        receiverCellBlockedCalls = new Label("0");
+        senderCellBlockedCalls = new Label("0");
+        locationBlockedCalls = new Label("0");
+        successfullyStartedCalls = new Label("0");
+        successfullyEndedCalls = new Label("0");
+
+        leftMenu.getChildren().addAll(successfullyStartedCalls, successfullyEndedCalls, locationBlockedCalls, senderCellBlockedCalls, receiverCellBlockedCalls);
+
+        return leftMenu;
+    }
+
+    private void setLabelValues(int successfullyStartedCalls, int successfullyEndedCalls, int locationBlockedCalls, int senderCellBlockedCalls, int receiverCellBlockedCalls){
+        this.successfullyStartedCalls.setText(successfullyStartedCalls + "");
+        this.successfullyEndedCalls.setText(successfullyEndedCalls + "");
+        this.locationBlockedCalls.setText(locationBlockedCalls + "");
+        this.senderCellBlockedCalls.setText(senderCellBlockedCalls + "");
+        this.receiverCellBlockedCalls.setText(receiverCellBlockedCalls + "");
+    }
+}
