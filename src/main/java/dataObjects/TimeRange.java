ce26004895badf93a0342a7eec8b55700bff9751
Thagus <agu.alcarin@gmail.com>
2016-11-28 10:24:38 -0600

    TimeRange class to store a range

---
+++ src/main/java/dataObjects/TimeRange.java
@@ -0,0 +1,48 @@
+package dataObjects;
+
+import java.sql.Time;
+import java.util.Objects;
+
+/**
+ * Created by Thagus on 27/11/16.
+ * Holds two times that define a range
+ */
+public class TimeRange {
+    private Time lowerBound;
+    private Time upperBound;
+
+    public TimeRange(Time lowerBound, Time upperBound) {
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+    }
+
+    public Time getLowerBound() {
+        return lowerBound;
+    }
+
+    public Time getUpperBound() {
+        return upperBound;
+    }
+
+    public boolean isWithinRange(Time timeToTest){
+        return timeToTest.after(lowerBound) && timeToTest.before(upperBound);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        TimeRange other = (TimeRange) obj;
+        return other.getLowerBound().equals(lowerBound) && other.getUpperBound().equals(upperBound);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.lowerBound, this.upperBound);
+    }
+}
