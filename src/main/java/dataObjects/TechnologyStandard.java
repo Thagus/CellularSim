c61c3ddd4f3d77264b705903a8f216356d654913
Thagus <agu.alcarin@gmail.com>
2016-11-28 13:11:43 -0600

    Technologies data holders

---
+++ src/main/java/dataObjects/TechnologyStandard.java
@@ -0,0 +1,12 @@
+package dataObjects;
+
+/**
+ * Created by Thagus on 28/11/16.
+ */
+public class TechnologyStandard {
+    private float cellRadius;
+    private int usersPerCell;
+    private float dataRate; //Data rate in kbps
+
+    private String multipleAccess;
+}
